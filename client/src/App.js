79ae15ea2dbf8d27e9f71863ba60f5b6b7f0d1f4
DimaKovalencko <71615244+DimaKovalencko@users.noreply.github.com>
2020-11-23 17:10:42 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">Cloud Storage</div>;
+}
+
+export default App;
---
+++ client/src/config.js
@@ -0,0 +1,1 @@
+export const API_URL = `https://guarded-retreat-78883.herokuapp.com/`;
